72f32247a3d79529bb144ab66c178c50ac5c1a7e
luminescify <ileahd96@gmail.com>
2021-11-23 17:18:21 -0500

    Updated footer links

--- Main/src/components/Footer/index.js
+++ Main/src/components/Footer/index.js
@@ -6,15 +6,15 @@ function Footer() {
   const icons = [
     {
       name: "fab fa-github",
-      link: "https://github.com/"
+      link: "https://github.com/luminescify"
     },
     {
       name: "fab fa-linkedin",
-      link: "https://www.linkedin.com/"
+      link: "https://www.linkedin.com/in/leahdavis96"
     },
     {
       name: "fab fa-stack-overflow",
-      link: "https://stackoverflow.com/"
+      link: "https://stackoverflow.com/users/16556767/leah-davis"
     }
   ]
 
